0673e457253e94b4cb7c2cca1de7e37ba228966a
kingwangzzang1234 <wychoi0408@naver.com>
2026-01-23 08:30:20 +0000

    feat: Upload Sample Resources

---
+++ python/260122001-comprehension.py
@@ -0,0 +1,17 @@
+# Comprehension
+# 존재하는 묶음 자료형에 일괄 작업을 수행하여 새로운 묶음 자료형을 만드는 것
+
+# Before comprehension
+nums = [1,2,3,4,5]
+result = []
+
+for i in nums:
+    result.append(i*2)
+
+# With comprehension
+nums = [1,2,3,4,5]
+result = [i * 2 for i in nums]
+
+# comprehension with if
+
+..
