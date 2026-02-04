29c58aaf7a39873a3fb9432df2061078d7481067
kingwangzzang1234 <wychoi0408@naver.com>
2026-02-04 08:37:00 +0000

    Add resources

---
+++ python/260203002-iteration.py
@@ -0,0 +1,15 @@
+# Iteration
+# 문장을 반복 수행하는 것
+
+# for
+
+# python은 for, range로 반복문을 수행
+for i in range(5):
+    print(i)
+
+# 내부변수가 필요없을 경우, _(ignore) 처리
+
+for _ in range(3):
+    print('hello')
+
+# while
